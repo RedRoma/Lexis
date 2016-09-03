b5b929e054aa606573279ab2b0e8607bfc6e70de
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-03 13:34:19 -0700

    Integrates with Sulcus and LexisDatabase

---
+++ Code/Lexis/LexisDatabase.framework/Headers/LexisDatabase.h
@@ -0,0 +1,19 @@
+//
+//  LexisDatabase.h
+//  LexisDatabase
+//
+//  Created by Wellington Moreno on 8/27/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LexisDatabase.
+FOUNDATION_EXPORT double LexisDatabaseVersionNumber;
+
+//! Project version string for LexisDatabase.
+FOUNDATION_EXPORT const unsigned char LexisDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LexisDatabase/PublicHeader.h>
+
+
