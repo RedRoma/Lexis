054af0c5cebf83876468fa1891e55ed2f77b7e1e
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-05 23:31:28 -0700

    Adds basic WelcomePageViewController

---
+++ Code/Lexis/Lexis/WelcomePageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomePageViewController.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/5/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+import Sulcus
+import RedRomaColors
+import UIKit
+
+class WelcomePageViewController: UIPageViewController
+{
+    
+    override func viewDidLoad()
+    {
+        
+    }
+}
