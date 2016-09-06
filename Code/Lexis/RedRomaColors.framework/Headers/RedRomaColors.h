ac12d98df6cecc706ee491abe52684ab651eb92b
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-05 23:19:21 -0700

    Adds RedRomaColors binary

---
+++ Code/Lexis/RedRomaColors.framework/Headers/RedRomaColors.h
@@ -0,0 +1,19 @@
+//
+//  RedRomaColors.h
+//  RedRomaColors
+//
+//  Created by Wellington Moreno on 9/5/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RedRomaColors.
+FOUNDATION_EXPORT double RedRomaColorsVersionNumber;
+
+//! Project version string for RedRomaColors.
+FOUNDATION_EXPORT const unsigned char RedRomaColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedRomaColors/PublicHeader.h>
+
+
