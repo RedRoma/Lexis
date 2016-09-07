70ca4f3ba25fafaa545cbd4c7320865cc97dacd6
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-06 23:56:45 -0700

    Adds Main.swift, Settings.swift
    + Removes vanilla ViewController

---
+++ Code/Lexis/Lexis/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/6/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+import Sulcus
+import UIKit
+
+class MainViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
