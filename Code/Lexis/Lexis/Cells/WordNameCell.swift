ff2fd1736bb945949a2779e2e6cd7a09318ac344
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-13 07:44:10 -0700

    Adds Word Cells

---
+++ Code/Lexis/Lexis/Cells/WordNameCell.swift
@@ -0,0 +1,15 @@
+//
+//  WordNameCell.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/13/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WordNameCell: UITableViewCell
+{
+    
+}
