5009b3e0f1f422334f0904a5e30c7c7820171f90
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-17 21:42:52 -0700

    Adds Arrays+ functionality

---
+++ Code/Lexis/Lexis/Arrays+.swift
@@ -0,0 +1,18 @@
+//
+//  Arrays+.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/17/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+
+
+extension Array
+{
+    var notEmpty: Bool
+    {
+        return !isEmpty
+    }
+}
