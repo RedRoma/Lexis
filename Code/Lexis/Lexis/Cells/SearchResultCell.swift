04362246f1a8e230e4511cc9dac29bf55054a9f5
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-17 19:52:12 -0700

    Adds source for Search Cells

---
+++ Code/Lexis/Lexis/Cells/SearchResultCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultCell.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/17/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchResultCell: UITableViewCell
+{
+    @IBOutlet weak var wordLabel: UILabel!
+    @IBOutlet weak var wordInformationLabel: UILabel!
+    
+}
