7c525c2a2277221159e27386554c6f427157a65b
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-20 23:06:15 -0700

    Updates dependencies

---
+++ Code/Lexis/AlchemyUIKit.framework/Headers/AlchemyUIKit.h
@@ -0,0 +1,19 @@
+//
+//  AlchemyUIKit.h
+//  AlchemyUIKit
+//
+//  Created by Wellington Moreno on 9/20/16.
+//  Copyright © 2016 Sir Wellington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlchemyUIKit.
+FOUNDATION_EXPORT double AlchemyUIKitVersionNumber;
+
+//! Project version string for AlchemyUIKit.
+FOUNDATION_EXPORT const unsigned char AlchemyUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemyUIKit/PublicHeader.h>
+
+
