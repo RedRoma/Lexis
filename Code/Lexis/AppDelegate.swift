ddfc0019e181bfcc3ff9ccf610d31ccb4fa8f8a6
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-26 07:20:37 -0700

    Adds Aroma message

--- Code/Lexis/AppDelegate.swift
+++ Code/Lexis/AppDelegate.swift
@@ -72,6 +72,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication)
+    {
+        AromaClient.beginMessage(withTitle: "Memory Warning")
+            .withPriority(.medium)
+            .addBody("Build #\(buildNumber)")
+            .send()
+    }
 
 }
 
