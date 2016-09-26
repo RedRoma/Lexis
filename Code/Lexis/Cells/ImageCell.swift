960d9b509c52be35cfa5938a7c58929924a39f7e
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-09-25 22:38:41 -0700

    Adds Storyboard Cell for Image results
    + Adds Sample Imags

---
+++ Code/Lexis/Cells/ImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCell.swift
+//  Lexis
+//
+//  Created by Wellington Moreno on 9/25/16.
+//  Copyright © 2016 RedRoma, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCell: UITableViewCell
+{
+    @IBOutlet weak var cardView: LexisView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
