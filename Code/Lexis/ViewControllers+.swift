45ed41766c55c85f19ff60f01e04093c813cf641
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-10-31 22:13:42 -0700

    Removes extraneous code

--- Code/Lexis/ViewControllers+.swift
+++ Code/Lexis/ViewControllers+.swift
@@ -17,22 +17,6 @@ extension UIViewController
     func hideNetworkIndicator()
     {
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
-        
-        //34.018132, -118.493179
-//        let link = "https://api.yelp.com/v3/businesses/search?term=delis&latitude=34.0181&longitude=-118.493179"
-//        let url = URL(string:link)!
-//        let token = "otPOIcrNXLV6Ak91B4Gs14YMvP5r5tH7eIZBPqjE2uk3h_lZ4McJGaxYJT4PJsoLlTfwEQ1svx15STNY6cxTpps3K1Yy4J4YkJIWqgbBp-7I06hI7Qsyk5YxSefeV3Yx"
-//        
-//        var request = URLRequest.init(url: url)
-//        request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-//        
-//        URLSession.shared.dataTask(with: request) { data, response, error  in
-//            
-//            guard let data = data else { return }
-//            //parse into json
-//            //update UI
-//        }.resume()
-        
     }
     
     func showNetworkIndicator()
